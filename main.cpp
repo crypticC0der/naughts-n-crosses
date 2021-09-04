d0964cf7f62dbcfe387a3a0acea8531ddea1688a
MJ Ponsonby <mj@crypticcoder.co.uk>
2021-09-04 12:32:16 +0100

    it works but right now its really bad, basically unrunnable after 3x3 grid size

--- main.cpp
+++ main.cpp
@@ -7,10 +7,18 @@
 #include <unistd.h>
 using namespace std;
 
-const int gridLen =3;
+const int gridLen =4;
+const int rowLen =4; //number in a row to win
 int gridSize;
 
-enum fillers{
+enum gameConditions{
+	win=1,
+	loose=-1,
+	draw=0,
+	notover=404
+};
+
+enum filler{
 	space,
 	naught,
 	cross,
@@ -19,7 +27,7 @@ enum fillers{
 
 char fillchars[] =  {' ','o','x','/'};
 
-fillers* grid; //yes im well aware im wasting bits here but fuck you, i cant be asked to code a better solution
+filler* grid; //yes im well aware im wasting bits here but fuck you, i cant be asked to code a better solution
 
 void drawString(float x, float y, char c) {
 	glRasterPos2f(x,y);
@@ -76,7 +84,152 @@ void disInit(){
 	glFlush();
 }
 
+bool hasWon(filler* cgrid,filler* win){
+	bool draw=true;
+	for(int i=0;i<gridLen;i++){
+		for(int j=0;j<gridLen;j++){
+			filler value = cgrid[i*gridLen + j];
+			draw = draw&&value!=space;
+			if(value==naught||value==cross){
+				*win=value;
+				int win=1;
+				int k=1;
+				//vertical
+				while(i+k<gridLen && i+rowLen<=gridLen){
+					if(cgrid[(i+k)*gridLen +j]==value){
+						win++;
+						k++;
+					}else{
+						break;
+					}
+				}
+				if(win>=rowLen){
+					return true;
+				}
 
+				win=1;
+				k=1;
+				//horiz
+				while(j+k<gridLen && j+rowLen<=gridLen){
+					if(cgrid[i*gridLen +(j+k)]==value){
+						win++;
+						k++;
+					}else{
+						break;
+					}
+				}
+				if(win>=rowLen){
+					return true;
+				}
+
+				win=1;
+				k=1;
+				//diagonal 1
+				while(j+k<gridLen && j+rowLen<=gridLen && i+k<gridLen && i+rowLen<=gridLen){
+					if(cgrid[(i+k)*gridLen +(j+k)]==value){
+						win++;
+						k++;
+					}else{
+						break;
+					}
+				}
+				if(win>=rowLen){
+					return true;
+				}
+
+				win=1;
+				k=1;
+				//diagonal 2
+				while(j-k>=0&& j-rowLen>=-1&& i+k<gridLen && i+rowLen<=gridLen){
+					if(cgrid[(i+k)*gridLen +(j-k)]==value){
+						win++;
+						k++;
+					}else{
+						break;
+					}
+				}
+				if(win>=rowLen){
+					return true;
+				}
+			}
+		}
+	}
+	*win=space;
+	return draw;
+}
+
+gameConditions intWon(filler* cgrid,filler side,filler enemy){
+	filler output;
+	bool won = hasWon(cgrid,&output);
+	if (won){
+		if(output==side){
+			return win;
+		}
+		else if(output==enemy){
+			return loose;
+		}
+		else{
+			return draw;
+		}
+	}
+	return notover;
+}
+
+float moveCalc(filler* cgrid,filler side,filler enemy){
+	//check if game over
+	gameConditions cond = intWon(cgrid,side,enemy);
+	if(cond!=notover){
+		return cond;
+	}
+
+	//continue game
+	int tryCount=0;
+	float trySum=0;
+	for(int i =0;i<gridSize;i++){
+		if(cgrid[i]==space){
+			//attempt
+			cgrid[i]=enemy;
+			
+			//check game state
+			gameConditions cond = intWon(cgrid,side,enemy);
+			if(cond!=notover){
+				cgrid[i]=space;
+				return cond;
+			}
+
+			//try for enempy moves
+			for(int j =0;j<gridSize;j++){
+				if(cgrid[j]==space){
+					cgrid[j]=side;
+					trySum+=moveCalc(cgrid,side,enemy);
+					tryCount+=1;
+					cgrid[j]=space;
+				}
+			}
+			cgrid[i]=space;
+		}
+	}
+	return trySum/tryCount;
+}
+
+void move(filler* cgrid,filler side, filler enemy){
+	int maxpos = 0;
+	float maxscore=-10000;
+	for(int i =0;i<gridSize;i++){
+		if(cgrid[i]==space){
+			cgrid[i]=side;
+			cout << "testing " << i << endl;
+			float out = moveCalc(cgrid,side,enemy);
+			if(out>=maxscore){
+				maxpos=i;
+				maxscore=out;
+			}
+			cgrid[i]=space;
+		}
+	}
+	cgrid[maxpos]=side;
+	cout << maxpos<<endl;
+}
 
 void run(){
 }
@@ -85,10 +238,23 @@ int main(int argc, char** argv) {
 	srand (time(NULL));
 	gridSize=gridLen*gridLen;
 	//while(!generateGrid()){}
-	grid=new fillers[gridSize];
+	filler* cgrid=new filler[gridSize];
 	for(int i =0;i<gridSize;i++){
-		grid[i]=naught;
+		cgrid[i]=space;
 	}
+	filler winner;
+	filler me,enemy;
+	me=cross;
+	enemy=naught;
+	while(!hasWon(cgrid,&winner)){
+		cout << "make a move" << endl;
+		move(cgrid,me,enemy);
+		filler t=me;
+		me=enemy;
+		enemy=t;
+	}
+	grid=cgrid;
+	cout << winner<<endl;
 	
 	glutInit(&argc, argv);		// Initialize GLUT
     glutInitWindowSize(300,300);   // Set the window's initial width & height
